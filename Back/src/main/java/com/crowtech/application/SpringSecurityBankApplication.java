307af036c47af6e7fc04844dd1c01af2c629283b
Thiago Oliveira <thiagop_o@hotmail.com>
2021-08-27 23:33:00 -0300

    Arrumado as pastas e Adicionado o Front

---
+++ Back/src/main/java/com/crowtech/application/SpringSecurityBankApplication.java
@@ -0,0 +1,20 @@
+package com.crowtech.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScans({ @ComponentScan("com.crowtech.controllers"), @ComponentScan("com.crowtech.config")  })
+@EnableJpaRepositories("com.crowtech.repositories")
+@EntityScan("com.crowtech.models")
+public class SpringSecurityBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBankApplication.class, args);
+	}
+
+}
---
+++ Back/src/main/java/com/crowtech/config/ProjectSecurityConfig.java
@@ -0,0 +1,110 @@
+package com.crowtech.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
+    //Configurando User com InMemoryAuthentication
+    /*@Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("12345").authorities("admin").and()
+                .withUser("user").password("12345").authorities("read").and()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+     */
+    /*
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws  Exception{
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails user = User.withUsername("admin").password("12345").authorities("admin").build();
+        UserDetails user1 = User.withUsername("user").password("12345").authorities("read").build();
+        userDetailsService.createUser(user);
+        userDetailsService.createUser(user1);
+        auth.userDetailsService(userDetailsService);
+    }
+
+     */
+
+    /*@Bean
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }*/
+
+    /**
+     * myAcccount = secured
+     * myBalance = secured
+     * myLoans = secured
+     * myCards = secured
+     * notices = not secured
+     * contact = not secured
+     */
+
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //Default Configuration
+        //http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
+
+        //Custom Configurations
+
+        http.authorizeRequests()
+                .antMatchers("/myAccount").authenticated()
+                .antMatchers("/myBalance").authenticated()
+                .antMatchers("/myLoans").authenticated()
+                .antMatchers("/myCards").authenticated()
+                .antMatchers("/notices").permitAll()
+                .antMatchers("/contact").permitAll()
+                .and()
+                .formLogin().and().httpBasic();
+
+
+
+        //Deny Configuration
+        /*
+        http.authorizeRequests()
+                .anyRequest().denyAll()
+                .and()
+                .formLogin().and()
+                .httpBasic();
+         */
+
+        //Permit all configuration
+        /*http
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin().and()
+                .httpBasic();
+
+         */
+
+    }
+
+    /*@Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }*/
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+
+    }
+}
